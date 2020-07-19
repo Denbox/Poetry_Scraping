cc752b03fb3b7a0ae0d8ff82ffa1738e406b0f69
Jacob Denbeaux <denbeaux@mit.edu>
2020-07-19 18:36:11 -0400

    finished metadata scraper

--- scrape_metadata.py
+++ scrape_metadata.py
@@ -1,6 +1,7 @@
 import os, requests
-from lxml import html
+from lxml import html, etree
 from utils import get_page, get_page_identifier, get_url_from_identifier, delay
+from functools import reduce
 
 # this time, instead of paginating, let's use the files in PNGs (run scrape_pngs.py before scrape_metada)
 # these scripts were built separately because I didn't know we needed the metadata at first
@@ -14,23 +15,39 @@ def filename_to_url(filename):
 def strip_ending(filename):
     return filename.split('.')[0]
 
+def name_score(name):
+    return reduce(lambda acc, next: acc * 1000 + int(next), name.split('_'), 1)
+
+
 # we will save our metadata in a Metadata directory. Recall that the PNGs are in the PNGs directory.
 def remaining_urls():
     png_filenames = map(strip_ending, os.listdir('PNGs'))
     metadata_filenames = map(strip_ending, os.listdir('Metadata'))
-    remaining_filenames = set(png_filenames) - set(metadata_filenames)
+    remaining_filenames = sorted(set(png_filenames) - set(metadata_filenames), key=name_score)
     return list(map(filename_to_url, remaining_filenames))
 
-def save_metadata(page):
-    raise NotImplementedError
+
+# we already have the page
+# figure out what part is metadata
+# save it to a file
+def capture_metadata(page):
+    class_to_grab = 'c-assetStack-auxiliary'
+    metadata_elements = page.xpath('//div[contains(@class, \'{}\')]'.format(class_to_grab))
+    return str([etree.tostring(i) for i in metadata_elements])
+
+def save_captured_data(metadata, id):
+    with open(os.path.join('Metadata', '{}_{}_{}.txt').format(id['volume'], id['issue'], id['page']), 'w') as f:
+        f.write(metadata)
 
 def scrape():
     for url in remaining_urls():
         try:
-            print(url)
+        # print(url)
             id = get_page_identifier(url)
             page = get_page(url)
-            save_metadata(page)
+            metadata_to_save = capture_metadata(page)
+            save_captured_data(metadata_to_save, id)
+            print('Saved volume {}, issue {}, page {}'.format(id['volume'], id['issue'], id['page']))
         except Exception as error:
             print('Failure at volume {}, issue {}, page {}: {}'.format(id['volume'], id['issue'], id['page'], error))
             print('Skipping for now')
